406b28dcb10cdc81ce966a2146ff9b4d92ba2fc0
trikop <trikopgaming@gmail.com>
2023-06-09 19:42:22 +0200

    clean

--- settings.lua
+++ settings.lua
@@ -1,17 +1,17 @@
 data:extend({
-    {
-		type = "string-setting",
-		name = "bismuth-disable-pcb-solder",
-		setting_type = "startup",
-		default_value = "Remove none (reccomended)",
+      {
+		    type = "string-setting",
+		    name = "bismuth-disable-pcb-solder",
+		    setting_type = "startup",
+		    default_value = "Remove none (reccomended)",
         allowed_values = {"Remove all (not reccomended)", "Remove most (works)", "Remove none (reccomended)"},
         order = "aaa"
       },
       {
-		type = "bool-setting",
-		name = "bismuth-disable-bismuthinite-ore",
-		setting_type = "startup",
-		default_value = false,
+        type = "bool-setting",
+        name = "bismuth-disable-bismuthinite-ore",
+        setting_type = "startup",
+        default_value = false,
         order = "aab"
       },
       {
@@ -23,20 +23,20 @@ data:extend({
       },
       {
         type = "int-setting",
-		name = "bismuth-contain-bismuth",
-		setting_type = "startup",
+        name = "bismuth-contain-bismuth",
+        setting_type = "startup",
         minimum_value = 1,
         maximum_value = 10000,
-		default_value = 8129,
+        default_value = 8129,
         order = "aad"
       },
       {
         type = "int-setting",
-		name = "bismuth-contain-sulfur",
-		setting_type = "startup",
+        name = "bismuth-contain-sulfur",
+        setting_type = "startup",
         minimum_value = 1,
         maximum_value = 10000,
-		default_value = 1871,
+        default_value = 1871,
         order = "aae"
       },
     })
