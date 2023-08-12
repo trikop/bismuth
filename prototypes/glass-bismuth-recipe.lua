9cdde65e65cbcbcfd2c6a2b424997eb192a123d2
trikop <trikopgaming@gmail.com>
2023-08-12 23:53:01 +0200

    0.1.6

--- prototypes/glass-bismuth-recipe.lua
+++ prototypes/glass-bismuth-recipe.lua
@@ -32,7 +32,7 @@ data:extend({
     enabled = false,
     energy_required = 1,
     ingredients = glass_ingredients, 
-    results = {{type = "item", name = "bismuth-glass", amount = 1, probability = 0.6}},
+    results = {{type = "item", name = "bismuth-glass", amount = 2}},
 },
 {
     type = "technology",
@@ -50,10 +50,6 @@ data:extend({
 },
 })
 
-if mods.bzfoundry and data.raw.item["foundry"] then
-  util.set_to_founding("bismuth-glass", {force=true})
-end
-
 util.add_prerequisite("bismuth-glass", "silica-processing")
 util.add_prerequisite("military-science-pack", "bismuth-glass")
 util.add_prerequisite("chemical-science-pack", "bismuth-glass")
@@ -64,20 +60,52 @@ util.add_ingredient("satellite", "bismuth-glass", 100)
 util.remove_ingredient("satellite", "glass")
 
 if mods.bzcarbon then
-    util.add_ingredient("low-density-structure-nanotubes", "bismuth-glass", 4)
+  util.add_ingredient("low-density-structure-nanotubes", "bismuth-glass", 4)
 end
 
 if mods["space-exploration"] then
-    util.add_ingredient("se-low-density-structure-beryllium", "bismuth-glass", 2)
-    util.remove_ingredient("se-low-density-structure-beryllium", "glass")
-    util.add_ingredient("se-spaceship-console", "bismuth-glass", 20)
-    util.remove_ingredient("se-spaceship-console", "glass")
-    util.add_ingredient("se-space-laser-laboratory", "bismuth-glass", 80)
-    util.remove_ingredient("se-space-laser-laboratory", "glass")
-    util.add_ingredient("se-space-radiation-laboratory", "bismuth-glass", 80)
-    util.remove_ingredient("se-space-radiation-laboratory", "glass")
-    util.add_ingredient("se-space-telescope", "bismuth-glass", 40)
-    util.remove_ingredient("se-space-telescope", "glass")
-    util.add_ingredient("se-space-telescope-microwave", "bismuth-glass", 80)
-    util.remove_ingredient("se-space-telescope-microwave", "glass")
+  if mods.bzsilicon and mods.calcium then
+    glass_ingredients = {{"sand", 22}, {"silica", 4}, {"bismuth-plate", 2}, {"calcium-plate", 1}}
+  elseif mods.bzsilicon then
+    glass_ingredients = {{"sand", 22}, {"silica", 4}, {"bismuth-plate", 2}}
+  else
+    glass_ingredients = {{"sand", 26}, {"bismuth-plate", 2}}
+  end
+  data:extend({
+  {
+      type = "recipe",
+      name = "se-bismuth-glass",
+      category = "crafting",
+      order = "d[bismuth-glass]",
+      enabled = false,
+      energy_required = 1,
+      ingredients = glass_ingredients, 
+      results = {{type = "item", name = "bismuth-glass", amount = 7}},
+      icons =
+      {
+        { icon = icon, icon_size = 128},
+        { icon = "__space-exploration-graphics__/graphics/icons/fluid/pyroflux.png", icon_size = 64, scale=0.25, shift= {-10, -10}},
+      },
+  },
+  })
+  util.add_effect("se-pyroflux-smelting", {type = "unlock-recipe", recipe= "se-bismuth-glass"})
+  util.add_ingredient("se-bismuth-glass", {type = "fluid", name = "se-pyroflux", amount = 10})
+
+  util.add_ingredient("se-low-density-structure-beryllium", "bismuth-glass", 2)
+  util.remove_ingredient("se-low-density-structure-beryllium", "glass")
+  util.add_ingredient("se-spaceship-console", "bismuth-glass", 20)
+  util.remove_ingredient("se-spaceship-console", "glass")
+  util.add_ingredient("se-space-laser-laboratory", "bismuth-glass", 80)
+  util.remove_ingredient("se-space-laser-laboratory", "glass")
+  util.add_ingredient("se-space-radiation-laboratory", "bismuth-glass", 80)
+  util.remove_ingredient("se-space-radiation-laboratory", "glass")
+  util.add_ingredient("se-space-telescope", "bismuth-glass", 40)
+  util.remove_ingredient("se-space-telescope", "glass")
+  util.add_ingredient("se-space-telescope-microwave", "bismuth-glass", 80)
+  util.remove_ingredient("se-space-telescope-microwave", "glass")
+end
+
+if mods.bzfoundry and data.raw.item["foundry"] then
+  util.set_to_founding("bismuth-glass", {force=true})
+  util.set_to_founding("se-bismuth-glass", {force=true})
 end
--- prototypes/glass-final-fixes.lua
+++ prototypes/glass-final-fixes.lua
@@ -1,5 +1,5 @@
 local util = require("data-util");
 
-util.add_ingredient("military-science-pack", "bismuth-glass", 1)
 util.add_ingredient("chemical-science-pack", "bismuth-glass", 10)
+util.add_ingredient("military-science-pack", "bismuth-glass", 1)
 util.remove_ingredient("chemical-science-pack", "glass")
