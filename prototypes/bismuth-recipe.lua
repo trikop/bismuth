692399cc3a306dfe4b2125bf67f8cb0f38d1dfcf
trikop <trikopgaming@gmail.com>
2025-09-23 13:55:56 +0200

    Update bismuth-recipe.lua

--- prototypes/bismuth-recipe.lua
+++ prototypes/bismuth-recipe.lua
@@ -1,6 +1,7 @@
 -- bismuth smelting
 
 local util = require("__bismuth__.data-util");
+local item_sounds = require('__base__.prototypes.item_sounds')
 
 if (not mods["pyrawores"] and not mods["bobplates"] and not mods["angelssmelting"] and not mods["IndustrialRevolution"]) then
 data:extend({
@@ -15,38 +16,27 @@ data:extend({
           { icon = "__bismuth__/graphics/icons/bismuth-ore.png", icon_size = 64, icon_mipmaps = 3, scale=0.25, shift= {-8, -8}},
         } or {
           { icon = "__bismuth__/graphics/icons/bismuth-plate.png", icon_size = 64, icon_mipmaps = 3, },
-        }
-),
-    normal = (mods["Krastorio2"] and
+        }),
+    main_product = util.me.bismuth_plate,
+    mods["Krastorio2"] and
+         {
+           enabled = true,
+           energy_required = 9,
+           ingredients = {util.item("bismuth-ore", 10)},
+           results = {
+             {type="item", name = util.me.bismuth_plate, amount=10},
+           },
+         } or
         {
-          main_product = util.me.bismuth_plate,
           enabled = true,
-          energy_required = 9,
-          ingredients = {{type="item", name="bismuth-ore", amount=10}},
-          results = {
-            {type="item", name = util.me.bismuth_plate, amount=10},
-          },
-        } or
-        {
-          main_product = util.me.bismuth_plate,
-          enabled = true,
+          allow_productivity = true,
           energy_required = 0.9,
-          ingredients = {{type="item", name="bismuth-ore", amount=1}},
+          ingredients = {util.item("bismuth-ore", 1)},
           results = {
             {type="item", name = util.me.bismuth_plate, amount=1},
           },
-        }),
-    expensive =
-    {
-      main_product = util.me.bismuth_plate,
-      enabled = true,
-      energy_required = 12.8,
-      ingredients = {{type="item", name="bismuth-ore", amount=4}},
-      results = {
-        {type="item", name = util.me.bismuth_plate, amount=2},
-      },
-    }
-  },
+        },
+    },
   {
     type = "item",
     name = util.me.bismuth_plate,
@@ -54,7 +44,11 @@ data:extend({
     icon_size = 64, icon_mipmaps = 3,
     subgroup = "raw-material",
     order = "b[bismuth-plate]",
-    stack_size = 100
+    stack_size = 100,
+    weight = 2*kg,
+    inventory_move_sound = item_sounds.metal_small_inventory_move,
+    pick_sound = item_sounds.metal_small_inventory_pickup,
+    drop_sound = item_sounds.metal_small_inventory_move,
   },
   mods["TheBigFurnace"] and {
     type = "recipe",
